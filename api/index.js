f9404d495f2f38d148e8842b539ced5034f0274c
Nicolás J. Engler <nicojengler@gmail.com>
2023-10-11 20:14:35 -0300

    Remove file generation and respond XML directly in response

--- api/index.js
+++ api/index.js
@@ -5,15 +5,18 @@ const RSS = require('rss');
 const fs = require('fs');
 const path = require('path');
 
-const { PROJECT_URL } = process.env;
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Function to create an RSS feed
 function createRSSFeed(url, feedTitle, feedDescription) {
-  request(url, (error, response, html) => {
-    if (!error && response.statusCode === 200) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (error || response.statusCode !== 200) {
+        reject(error || `Request failed with status code ${response.statusCode}`);
+        return;
+      }
+
       const $ = cheerio.load(html);
       const feed = new RSS({
         title: feedTitle,
@@ -37,21 +40,26 @@ function createRSSFeed(url, feedTitle, feedDescription) {
       });
 
       const xml = feed.xml({ indent: true });
-      const parentDirectory = path.join(__dirname, '..'); // Resolve the parent directory
-      fs.writeFileSync(path.join(parentDirectory, 'public/rss.xml'), xml);
-    }
+      resolve(xml);
+    });
   });
 }
 
-// Example usage
-app.get('/api/generate-rss', (req, res) => {
-  createRSSFeed(
-    'https://www.goodreads.com/news',
-    'Goodreads RSS Feed',
-    'Latest articles from Goodreads'
-  );
+// Get feed
+app.get('/api/rss', async (req, res) => {
+  try {
+    const feed = await createRSSFeed(
+      'https://www.goodreads.com/news',
+      'Goodreads RSS Feed',
+      'Latest articles from Goodreads'
+    );
 
-  res.status(200).send('RSS feed generated successfully!');
+    res.header('Content-Type', 'application/xml');
+    res.send(feed);
+  } catch (error) {
+    console.error('Error generating or sending RSS feed:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(port, () => {
