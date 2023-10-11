b7cb3d103ef4b6ce21a3f2366fa75001880a7a36
Nicolás J. Engler <nicojengler@gmail.com>
2023-10-11 19:35:31 -0300

    Add express API endpoint to generate RSS feed

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
+const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
 const RSS = require('rss');
+const fs = require('fs');
 
 const { PROJECT_URL } = process.env;
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Function to create an RSS feed
 function createRSSFeed(url, feedTitle, feedDescription) {
-  // Fetch the HTML content of the website
   request(url, (error, response, html) => {
     if (!error && response.statusCode === 200) {
-      // Parse the HTML using Cheerio
       const $ = cheerio.load(html);
-
-      // Create an RSS feed
       const feed = new RSS({
         title: feedTitle,
         description: feedDescription,
-        feed_url: `${PROJECT_URL}/public/rss.xml`,
+        feed_url: `${url}/rss.xml`,
         site_url: url,
       });
 
-      // Extract relevant information from the website
-      $('.elementListLast.mediumText--index').each((index, element) => {
+      $('.editorialCard').each((index, element) => {
         const title = $(element).find('.editorialCard__title').text();
         const link = $(element).find('.editorialCard__title .gr-hyperlink').attr('href');
         const description = $(element).find('.editorialCard__body').text();
         const pubDate = $(element).find('.editorialCard__timestamp').text();
 
-        // Add the item to the RSS feed
         feed.item({
           title: title,
           description: description,
@@ -35,16 +35,23 @@ function createRSSFeed(url, feedTitle, feedDescription) {
         });
       });
 
-      // Save the RSS feed to a file
       const xml = feed.xml({ indent: true });
-      require('fs').writeFileSync('public/rss.xml', xml);
+      fs.writeFileSync('public/rss.xml', xml);
     }
   });
 }
 
 // Example usage
-createRSSFeed(
-  'https://www.goodreads.com/news',
-  'Goodreads RSS Feed',
-  'Latest articles from Goodreads'
-);
+app.get('/generate-rss', (req, res) => {
+  createRSSFeed(
+    'https://www.goodreads.com/news',
+    'Goodreads RSS Feed',
+    'Latest articles from Goodreads'
+  );
+
+  res.status(200).send('RSS feed generated successfully!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
